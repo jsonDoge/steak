f94c937f14223a348e78ea227767144d2b1ff519
Matas555 <matas.jasonas@gmail.com>
2023-02-01 17:04:39 +0200

    feat: adding linting configurations and lint all files

--- contracts/Stake.sol
+++ contracts/Stake.sol
@@ -5,15 +5,17 @@ pragma solidity ^0.8;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Stake {
+    address private stakeToken;
 
-  address stakeToken;
+    constructor(address stakeToken_) {
+        stakeToken = stakeToken_;
+    }
 
-  constructor (address _stakeToken) {
-    stakeToken = _stakeToken;
-  }
-
-  function stake (uint256 amount) public {
-    require(amount > 0, "CANT_STAKE_ZERO_AMOUNT");
-    require(IERC20(stakeToken).allowance(msg.sender, address(this)) >= amount, "NOT_ENOUGH_ALLOWANCE");
-  }
+    function stake(uint256 amount) public view {
+        require(amount > 0, "CANT_STAKE_ZERO_AMOUNT");
+        require(
+            IERC20(stakeToken).allowance(msg.sender, address(this)) >= amount,
+            "NOT_ENOUGH_ALLOWANCE"
+        );
+    }
 }
