619a722cd57db2145b1732305e890ce77d3ce632
Matas555 <matas.jasonas@gmail.com>
2023-02-01 15:24:23 +0200

    refactor: add helper function waitTx for awaiting transactions

--- test/stake.test.js
+++ test/stake.test.js
@@ -1,6 +1,7 @@
 const { expect } = require('chai');
 
 const { setupContracts } = require('./helpers/setup');
+const { waitTx } = require('./helpers/utils');
 
 let contracts;
 let account;
@@ -18,8 +19,7 @@ describe('App helper functions', function () {
     it('Try to stake without setting allowance', async function () {
       const stakeAmount = 1;
       try {
-        const tx = await contracts.stake.connect(account).stake(stakeAmount);
-        await tx.wait();
+        await waitTx(contracts.stake.connect(account).stake(stakeAmount));
       } catch (e) {
         expect(e.toString()).to.contain('NOT_ENOUGH_ALLOWANCE');
       }
